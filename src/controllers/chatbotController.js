1ec5c569b7660babdab327d03df980ca33200621
quybitch <dunguyenquy@gmail.com>
2025-08-13 13:57:38 +0700

    First commit

---
+++ src/controllers/chatbotController.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+
+let getHomePage = (req,res) => {
+    return res.send('Hello World!');
+
+}
+let getWebhook = (req,res) => {
+    let VERIFI_TOKEN = process.env.VERIFI_TOKEN;
+
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+
+    // Check if a token and mode is in the query string of the request
+    if (mode && token) {
+        // Check the mode and token sent is correct
+        if (mode === "subscribe" && token === VERIFI_TOKEN) {
+            // Respond with the challenge token from the request
+            console.log("WEBHOOK_VERIFIED");
+            res.status(200).send(challenge);
+        } else {
+            // Respond with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
+}
+let postWebhook = (req,res) => {
+    let body = req.body;
+
+    console.log(`\u{1F7EA} Received webhook:`);
+    console.dir(body, { depth: null });
+    if (body.object === "page") {
+        // Returns a '200 OK' response to all requests
+        res.status(200).send("EVENT_RECEIVED");
+        // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
+    } else {
+        res.sendStatus(404);
+    }
+}
+module.exports = {
+    getHomePage: getHomePage,
+    getWebhook: getWebhook,
+    postWebhook: postWebhook
+}
---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/web";
+import bodyParser from "body-parser";
+require("dotenv").config();
+
+let app = express();
+//config view engine
+viewEngine(app);
+
+// parse request to json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// init web routes
+initWebRoutes(app);
+
+let port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
