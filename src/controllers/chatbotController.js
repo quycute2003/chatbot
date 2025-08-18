9f1e8cf1073419cc7dcf7ec262a49e4becba563b
quybitch <dunguyenquy@gmail.com>
2025-08-18 10:16:52 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const VERIFI_TOKEN = process.env.VERIFI_TOKEN;
 let getHomePage = (req,res) => {
     return res.send('Hello World!');
 
@@ -33,10 +35,35 @@ let postWebhook = (req,res) => {
         // Returns a '200 OK' response to all requests
         res.status(200).send("EVENT_RECEIVED");
         // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
+        body.entry.forEach(entry => {
+            //gets the body of the webhook event
+            let webhook_event= entry.messaging[0];
+            console.log(webhook_event);
+
+            //get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID:'+ sender_psid);
+        })
     } else {
         res.sendStatus(404);
     }
 }
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+//Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+//Sends response messages via the send API
+function callSendAPI(sender_psid, response) {
+
+}
+
+
 module.exports = {
     getHomePage: getHomePage,
     getWebhook: getWebhook,
